e6a4f58b330ff236e580225d83114ac0034f435d
h.sheikhi <@h.sheikhi>
2023-05-29 14:16:18 +0330

    first commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">About Page</h1>
+        <h2 className="text-1xl font-bold">Welcome to My React App</h2>
+        <p className="mt-2">This is a sample component using Tailwind CSS.</p>
+        <button className="bg-green-500 text-white px-4 py-2 mt-4 rounded">
+          Click me!
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default About;
